e0123331a5ecaa6702207219085d97821a6c4e92
kdevivanco <kdevivanco@gmail.com>
2023-03-31 07:37:15 -0500

    lab 1

--- lab_1/lab1_30_03_2023.cpp
+++ lab_1/lab1_30_03_2023.cpp
@@ -5,9 +5,20 @@ using namespace std;
 int main(){
     Text::Text() = default; //constructor por defecto, tambien puede ser Text::Text(){}
     // Constructor por parametros
-    Text::Text(const char* texto); //declaracion
+    Text::Text(const char* text){
+        _sz = strlen(text);
+        _text = new char[_sz+1];
+        strcpy(_text,text);
+    }
     //Constructor copia 
-    Text::Text(const Text& other);
+    Text::Text(const Text& other){
+        if(&other == this){
+            return;
+        }
+        _sz = other._sz;
+        _text = new char[_sz +1];
+        strcpy(_text;other._text);
+    }
     //Constructor move
     Text::Text(Text&&other);
     //Asignacion copia sobre carga de operadores
